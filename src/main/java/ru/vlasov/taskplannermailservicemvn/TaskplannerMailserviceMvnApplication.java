bcac1734e13bde26060a65c7ec8a4d1e5cfc4dc6
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-11-19 11:55:01 +0300

    feat: init commit, replace project from gradle to maven

---
+++ src/main/java/ru/vlasov/taskplannermailservicemvn/TaskplannerMailserviceMvnApplication.java
@@ -0,0 +1,13 @@
+package ru.vlasov.taskplannermailservicemvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplannerMailserviceMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskplannerMailserviceMvnApplication.class, args);
+    }
+
+}
